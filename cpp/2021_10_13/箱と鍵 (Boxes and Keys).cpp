14acfcc85489aa178027e8e9886bcd183346378d
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-10-13 18:29:17 +0900

    Add files via upload

--- cpp/2021_10_13/箱と鍵 (Boxes and Keys).cpp
+++ cpp/2021_10_13/箱と鍵 (Boxes and Keys).cpp
@@ -5,24 +5,54 @@ using namespace std;
 
 // input
 // (N個の宝箱) (M個の鍵)
-// (N個の整数A A1,A2,...,Ai)
-// (M個の整数B B1,B2,...,Bj)
+// (箱それぞれに書かれた整数N個)
+// (鍵それぞれに書かれた整数M個)
 
 // output
 // 開けられる宝箱数 のmax
 
 int main(){
-    int NumOfChests,NumOfKeys;
-    int Chests[NumOfChests];
-    int Keys[NumOfKeys];
-    for(int iInp=0;iInp<NumOfChests;iInp++){
-        cin >> Chests[iInp];
+    int numOfChests,numOfKeys;
+    cin >> numOfChests >> numOfKeys;
+    int chests[numOfChests];
+    for(int inp0=0;inp0<numOfChests;inp0++){
+        cin >> chests[inp0];
     }
-    for(int jInp=0;jInp<NumOfKeys;jInp++){
-        cin >> Keys[jInp];
+    int keys[numOfKeys];
+    for(int inp1=0;inp1<numOfKeys;inp1++){
+        cin >> keys[inp1];
     }
 
-    // 配列Keys中の要素の重複をなくす
-    
+    // 鍵1で何個，鍵2で何個... 既出はスキップ
 
+    int canOpen = 0;
+
+    // iはkeys
+    for(int i=0;i<numOfKeys;i++){
+        // keys[i]が既出 => skip
+        bool already = false;
+        for(int j=0;j<i;j++){
+            if(keys[i] == keys[j]){
+                already = true;
+                break;
+            }
+        }
+        if(already == true){
+            continue;
+        }
+
+        // kはchests
+        for(int k=0;k<numOfChests;k++){
+            if(keys[i] == chests[k]){
+                canOpen++;
+                // test
+                // cout << "keys[" << i << "] and chests[" << k << "] are same" << endl;
+            }
+        }
+
+    }
+
+    cout << canOpen << endl;
+
+    return 0;
 }
