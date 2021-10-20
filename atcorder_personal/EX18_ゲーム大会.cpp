81c74fe00b80cd8c618b490ffe1ca2c13ca6b66a
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-10-20 10:17:14 +0900

    Add files via upload

--- atcorder_personal/EX18_ゲーム大会.cpp
+++ atcorder_personal/EX18_ゲーム大会.cpp
@@ -1,2 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int a[m],b[m];
+    for(int i=0;i<m;i++){
+        cin >> a[i] >> b[i];
+    }
+
+    
+
+
+}
