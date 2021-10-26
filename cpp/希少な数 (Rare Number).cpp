4d281a9927193cd154cb71bcdf1e0ba229d54883
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-10-26 16:04:42 +0900

    Add files via upload

---
+++ cpp/希少な数 (Rare Number).cpp
@@ -0,0 +1,18 @@
+// https://www.ioi-jp.org/joi/2021/2022-yo1/2022-yo1b-t4.html
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    int A[N];
+    for(int inp1 = 0;inp1<N;inp++){
+        cin >> A[inp1];
+    }
+
+    // 1 <= A <= 2000
+    int counter[2001]; // 0 <= index <= 2000
+
+    
+}
