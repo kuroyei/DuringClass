aa45a1331022a282a4f42b5aaaecf10396059175
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-12-01 19:24:05 +0900

    Add files via upload

--- cpp/2021_12_01/AOJ0174.cpp
+++ cpp/2021_12_01/AOJ0174.cpp
@@ -10,9 +10,6 @@ int main(){
         int a = 0;
         int b = 0;
 
-        // A -> Aがサーブした, B -> Bがサーブした
-        // 出力はサーブした数ではなく得点である
-
         cin >> records;
 
         if(records[1] == '\0'){
@@ -25,18 +22,16 @@ int main(){
             }
         }
 
-	a = strlen(records) - b;
+	    a = strlen(records) - b;
 
         if((i%3 != 0)&&(a != 0)&&(b != 0)){
-		if(records[0] == 'A'){
-			b++;
-			a--;
-		} else {
-			a++;
-			b--;
-		
-        	}	
-
+            if(records[0] == 'A'){
+                b++;
+                a--;
+            } else {
+                a++;
+                b--;
+            }	
     	}
 
         cout << a << ' ' << b << endl;
@@ -45,60 +40,3 @@ int main(){
 	return 0;
 }
 
-/*
-ABAABBBAABABAAABBAA
-    start from A
-    1st Game (i == 0) *** (i%3 == 0)
-    N of A == 11, B == 8
-    output 11 8
-AABBBABBABBAAABABABAAB
-    start from A because end of 1st Game is A
-    2nd Game (i == 1)
-    N of A == 11, B == 11
-    output 10 12 (B++;A--;)
-BABAABAABABABBAAAB
-    Start from B because end of 2nd Game is B
-    3rd Game (i == 2)
-    N of A == 10, B == 8
-    output 11 7 (A++;B--;)
-AABABAAABBAABBBABAA
-    1st Game (i == 3) ***
-    N of A == 11, B == 8
-    output 11 8
-AAAAAAAAAAA
-    Start from A because end of 1st Game is A
-    2nd Game (i == 4)
-    N of A == 11, B == 0
-    output 11 0 (B == 0 -> do not touch)
-ABBBBBBBBBB
-    Start from A because end of 2nd Game is A
-    3rd Game (i == 5)
-    N of A == 1, B== 10
-    output 0 11 (B++;A--;)
-0
-
-When ***{
-    N of A, B == Output
-}
-else{
-    if(N of A != 0 || N of B != 0) 全部同じ文字であれば文字数に等
-
-        if(rec[0] == 'A'){
-            b++;
-            a--;
-        }
-        if(rec[0] == 'B'){
-            a++;
-            b--;
-        }
-
-
-}
-
-
-sum is number of times of played
-count number of B
-if(!(0 or 3の倍数) && (str[0] == A))
-    B++;
-
-*/
