c7c9e40d8b418c8abae25d55f8b9314c4da97caf
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-12-08 17:29:52 +0900

    Add files via upload

--- cpp/2021_12_08/Distance_II.cpp
+++ cpp/2021_12_08/Distance_II.cpp
@@ -4,35 +4,49 @@
 #include <cmath>
 using namespace std;
 
-int rthPower(int a,int r){
-    int ans = 1;
-    for(int i=0;i<r;i++){
-        ans*=a;
-    }
-    return ans;
-}
-
 int main(){
     // １乗根，２乗根，３乗根，無限乗根
     int num;
     cin >> num;
 
     // x座標配列
-    int* xCoordinates = new int[num];
+    int* xs = new int[num];
     for(int i=0;i<num;i++){
-        cin >> xCoordinates[i];
+        cin >> xs[i];
     }
 
     // y座標配列
-    int* yCoordinates = new int[num];
+    int* ys = new int[num];
     for(int i=0;i<num;i++){
-        cin >> yCoordinates[i];
+        cin >> ys[i];
     }
 
-    // １乗根
-    int insideRadical = 0;
+    // 絶対値
+    int* absolutes = new int[num];
+    int maxOfAbsolute;
     for(int i=0;i<num;i++){
-        insideRadical+=fabs(xCoordinates[i] - yCoordinates[i]);
+        absolutes[i] = fabs(xs[i] - ys[i]);
+
+        // p = infinite の為にmaxも求める
+        if(i==0){
+            maxOfAbsolute = absolutes[1];
+        } else if(maxOfAbsolute < absolutes[i]) {
+            maxOfAbsolute = absolutes[i];
+        }
     }
 
+    // 1,2,3乗根
+    for(double p=1;p<=3;p++){
+        int inRoot = 0;
+        for(int i=0;i<num;i++){
+            inRoot += pow(absolutes[i], p);
+        }
+
+        cout << pow(inRoot, 1/p) << endl;
+    }
+
+    // 無限乗根
+    cout << maxOfAbsolute << endl;
+
+    return 0;
 }
