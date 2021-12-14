d1dbc1af9af26caa4194fe5b1a8a0be6914aecc8
Kuroyei <81549868+kuroyei@users.noreply.github.com>
2021-12-15 07:44:50 +0900

    Add files via upload

---
+++ cpp/square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+inline int square(int x)
+{
+    return x*x;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << square(n) << endl;
+    return 0;
+}
